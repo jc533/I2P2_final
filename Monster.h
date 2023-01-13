77613f297e8aebef524af379f5a70ea6b39ca4fc
attackofthetitan <ethanlin.0902@gmail.com>
2023-01-13 13:28:00 +0800

    add monster
    changed detect
    cant run
    wait to detect player

--- Monster.h
+++ Monster.h
@@ -19,27 +19,33 @@ public:
 
     // Draw image per frame
     // override virtual function "Object::Draw"
-    void Draw();
+    void virtual Draw();
     // Load bitmaps of animation image into container "moveImg"
-    void Load_Move();
+    void virtual Load_Move();
 
     // Update monster position per frame
     // And detect if it reaches end point but not destroyed
-    bool Move();
+    bool virtual Move();
 
     // functions that return informations of monster
-    int getDir() { return direction; }
-    int getWorth() { return worth; }
-    int getScore() { return score; }
-
-    bool Subtract_HP(int);
+    int virtual getDir() { return direction; }
+    int virtual getWorth() { return worth; }
+    int virtual getScore() { return score; }
+    bool virtual Subtract_HP(int);
+    // update whole attack set
+    // detect if tower needs to attack some monster
+    bool virtual DetectAttack(Player*);
+    // process if some of attack in set touches monster
+    bool virtual TriggerAttack();
+    int virtual State();
 
 protected:
     int direction_count[4];
-    int HealthPoint = 20;
-    int speed = 1;
-    int worth = 10;
-    int score = 100;
+    int HealthPoint;
+    int speed;
+    int defense;
+    //int worth = 10;
+    //int score = 100;
     char class_name[20];
 private:
     // direction and index for "path"
