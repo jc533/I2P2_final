e3b2ec056b2b116ef31e3537392b91950873e380
unknown <yub612@gail.com>
2023-01-13 13:09:45 +0800

    add character

---
+++ character.h
@@ -0,0 +1,56 @@
+#ifndef CHARACTER_H_INCLUDED
+#define CHARACTER_H_INCLUDED
+
+#include <stdio.h>
+#include <vector>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_primitives.h>
+#include "Object.h"
+#include "Circle.h"
+#include "global.h"
+
+class Character : public Object{
+public:
+    Character(int, int);
+    virtual ~Character();
+
+    // override virtual function "Object::Draw"
+    void Draw();
+    //void resetAttackCounter() { attack_counter = 0; }
+
+    // update whole attack set
+    // if any attack goes out of the range of tower, delete it
+    // process if some of attack in set touches monster
+    virtual bool Update();
+    virtual bool Move();
+    virtual int Detect(Monster*);
+    virtual  bool Subtract_HP(int);
+
+    virtual int getWidth() { return 40; }
+    virtual int getHeight() { return 40; }
+
+    // show selected tower image on cursor position
+    //static void SelectedTower(int, int, int);
+
+protected:
+
+    enum class Type {ATTACK,DODGE,MOVE};
+    Type state;
+    int defense;
+    int speed;
+    int direction;
+    int direction_count[4];
+    int type;
+    int attack_frequency;
+    //int attack_counter;
+    int attack_harm_point;
+    //ALLEGRO_BITMAP *attack_img;
+
+    // information of tower
+    ALLEGRO_BITMAP std::vector<*img>;
+};
+
+#endif // CHARACTER_H_INCLUDED
---
+++ ninja.h
@@ -0,0 +1,29 @@
+#ifndef NINJA_H_INCLUDED
+#define NINJA_H_INCLUDED
+
+#include <stdio.h>
+#include <string>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_primitives.h>
+#include "Object.h"
+#include "Circle.h"
+#include "global.h"
+#include "character.h"
+
+class Ninja : public Character{
+public:
+    Ninja(int pos_x, int pos_y):Character(pos_x,pos_y){
+        defense=5;
+        speed=10;
+        attack_frequency=10;
+        attack_harm_point=5;
+        type="ninja";
+    };
+};
+
+
+
+#endif
