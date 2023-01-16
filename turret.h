b6bfc584be91c311f7431cba2eaf71870fe96086
jc533 <yub612@gmail.com>
2023-01-16 21:14:23 +0800

    add turret?

--- turret.h
+++ turret.h
@@ -2,17 +2,16 @@
 #define TURRET_H_INCLUDED
 
 #include <stdio.h>
-#include <string>
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>
 #include <allegro5/allegro_primitives.h>
 #include "Object.h"
+#include "Attack.h"
 #include "Circle.h"
-#include "global.h"
 #include "Monster.h"
-
+#include "global.h"
 
 class Turret : public Monster{
 public:
@@ -24,7 +23,34 @@ public:
         //class_name="turret";
         strncpy(class_name, "Wolf", 20);
     };
+    virtual ~Turret();
+
+    // override virtual function "Object::Draw"
+    void Draw();
+    void resetAttackCounter() { attack_counter = 0; }
+    void ToggleClicked() { isClicked = !isClicked; }
+
+    // update whole attack set
+    // if any attack goes out of the range of tower, delete it
+    void UpdateAttack();
+    // detect if tower needs to attack some monster
+    bool DetectAttack(Character*);
+    // process if some of attack in set touches monster
+    bool TriggerAttack(Character*);
+
+    int attack_frequency = 20;
+    int attack_counter = 0;
+    int attack_harm_point = 5;
+    int attack_velocity = 10;
+    ALLEGRO_BITMAP *attack_img;
+    std::vector<Attack*> attack_set;
+
+    // information of tower
+    int type;
+    bool isClicked = false;
+    ALLEGRO_BITMAP *img;
 };
+
 
 
 #endif // TURRET_H_INCLUDED
